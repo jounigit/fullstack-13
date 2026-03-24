f93b875b32ebe8783e181f2be3a3b975a2d62ca1
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2026-03-24 20:14:50 +0200

    16. Authors

---
+++ controllers/authors.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Blog, User } = require('../models');
+const { sequelize } = require('../util/db');
+
+router.get('/', async (req, res) => {
+  try {
+    const authors = await Blog.findAll({
+      attributes: [
+        'author',
+        [sequelize.fn('COUNT', sequelize.col('id')), 'articles'],
+        [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+      ],
+      group: ['author'],
+      order: [[sequelize.literal('likes'), 'DESC']]
+    });
+
+    res.json(authors);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
